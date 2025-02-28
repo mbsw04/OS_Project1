acea5eee7349bcd24e1d94dcbfabdc0e7930ccf7
Maan Bhagat <146051880+mbsw04@users.noreply.github.com>
2025-02-28 15:01:49 -0500

    Update B_Program1.cpp

--- B_Program1.cpp
+++ B_Program1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <unistd.h>
-#include <cstring>
+#include <sys/stat.h>
 
 using namespace std;
 
@@ -9,31 +9,33 @@ int main() {
     const char* atcPipe = "atc_pipe";
     const char* aircraftPipe = "aircraft_pipe";
 
-    // Open ATC pipe for writing
+    // Create named pipes if they don't exist
+    mkfifo(atcPipe, 0666);
+    mkfifo(aircraftPipe, 0666);
+
+    // Open ATC pipe for writing (send clearance)
     ofstream atcOut(atcPipe);
     if (!atcOut) {
-        cerr << "Error opening ATC pipe!" << endl;
+        cerr << "Error opening ATC pipe for writing!" << endl;
         return 1;
     }
 
-    // Send landing clearance
     string message = "ATC Tower: Permission granted to land.";
     cout << "ATC Tower: Sending clearance..." << endl;
     atcOut << message << endl;
-    atcOut.close();  // Close after writing
+    atcOut.close(); // Close after writing
 
     // Open aircraft pipe for reading response
     ifstream aircraftIn(aircraftPipe);
     if (!aircraftIn) {
-        cerr << "Error opening Aircraft pipe!" << endl;
+        cerr << "Error opening Aircraft pipe for reading!" << endl;
         return 1;
     }
 
-    // Read response from aircraft
     string response;
     getline(aircraftIn, response);
     cout << "ATC Tower received: " << response << endl;
-
     aircraftIn.close(); // Close after reading
+
     return 0;
 }
